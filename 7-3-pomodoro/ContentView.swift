88b3064270ee7d7d6b21f08f7de3a6c8745b9f2f
Adelina C <73216866+adelly13@users.noreply.github.com>
2021-07-03 14:11:30 -0700

    Added Start/Stop and Formatted Time

--- 7-3-pomodoro/ContentView.swift
+++ 7-3-pomodoro/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
+    var initialCountdown: Double = 10 // 10 minutes
+    
+    @State var receiver = Timer.publish(every: 1, on: .current, in: .default).autoconnect()
     @State var texts: String = "hello"
+    @State var seconds: Int = 0
+    @State var status: String = "Start"
+    @State var countdown: Double = 0
     
     var circleDiameter: CGFloat = UIScreen.main.bounds.width * 0.9
     
@@ -41,8 +47,34 @@ struct ContentView: View {
                 Rectangle()
                     .frame(width: 3, height: circleDiameter / 2)
                     .offset(x: 0, y: -circleDiameter / 4)
+                    .rotationEffect(Angle(degrees: Double(seconds*6)))
+            }
+            .onAppear() {
+                countdown = initialCountdown*60
+            }
+            .onReceive(receiver, perform: { _ in
+                if (countdown > 0 && status == "Stop") {
+                    withAnimation(Animation.linear(duration: 0.01)) {
+                        seconds += 1
+                    }
+                    countdown -= 1
+                }
+            })
+            Text(GetFormattedTime(iSeconds: countdown))
+            Button(status) {
+                if (status == "Start") {
+                    status = "Stop"
+                } else {
+                    status = "Start"
+                }
             }
         }
+    }
+    func GetFormattedTime(iSeconds: Double) -> String {
+        let minutes = Int(floor(iSeconds/60))
+        let seconds = Int(Int(iSeconds)%60)
+        
+        return String(minutes) + ":" + ((seconds < 10) ? "0" : "") + String(seconds)
     }
 }
 
