2f04898da9ae0e7a3bc88eb8b63a034a909b3967
simfony <49770335+wizard1238@users.noreply.github.com>
2021-07-03 11:08:49 -0700

    created thing

---
+++ 7-3-pomodoro/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  7-3-pomodoro
+//
+//  Created by Jeremy Tow on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var receiver = Timer.publish(every: 1, on: .current, in: .default).autoconnect()
+    @State var seconds: Int = 0;
+    @State var status: String = "Start"
+    
+    var circleDiameter: CGFloat = UIScreen.main.bounds.width * 0.9
+    
+    var rectHeight: CGFloat = 15
+    var rectWidthOffset: CGFloat = 75
+    var rectWidthMult: CGFloat = 8
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(0..<8) { i in
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width - rectWidthOffset) / rectWidthMult, height: rectHeight)
+                }
+            }
+            
+            ZStack {
+                Circle()
+                    .fill(Color.primary)
+                    .opacity(0.1)
+                    .frame(width: circleDiameter, height: circleDiameter, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                ForEach(0..<60) { i in
+                    Rectangle()
+                        .fill(Color.primary)
+                        .opacity(0.7)
+                        .offset(x: 0, y: -circleDiameter / 2.3)
+                        .rotationEffect(Angle(degrees: Double(i * 6)))
+                        .frame(width: 2, height: (i % 5 == 0) ? 20 : 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                Rectangle()
+                    .fill(Color.primary)
+                    .offset(x: 0, y: -circleDiameter / 4)
+                    .rotationEffect(Angle(degrees: Double(seconds * 6)))
+                    .frame(width: 3, height: circleDiameter / 2)
+            }
+            .onReceive(receiver, perform: { _ in
+                if status == "Stop" {
+                    withAnimation(Animation.linear(duration: 0.01)) {
+                        seconds += 1;
+                    }
+                }x
+                
+            })
+            
+            Button(status) {
+                if (status == "Start") {
+                    status = "Stop"
+                } else {
+                    status = "Start"
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 7-3-pomodoro/__3_pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  __3_pomodoroApp.swift
+//  7-3-pomodoro
+//
+//  Created by Jeremy Tow on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct __3_pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
